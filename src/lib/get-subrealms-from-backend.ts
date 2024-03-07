290ba0f217f0525499ea7989646e1be8590e393e
disco-quotidie <ccprasong@outlook.com>
2024-03-07 19:36:58 +0000

    error fix

--- src/lib/get-subrealms-from-backend.ts
+++ src/lib/get-subrealms-from-backend.ts
@@ -1,6 +1,6 @@
 import axios from "axios"
 
 export default async function getSubrealmsFromBackend (network: string) {
-  const response = await axios.get(`${process.env.NEXT_PUBLIC_BACKEND_APIENDPOINT}/api/get-subrealms`)
+  const response = await axios.get(`${process.env.NEXT_PUBLIC_BACKEND_APIENDPOINT}/get-subrealms`)
   return response.data
 }
