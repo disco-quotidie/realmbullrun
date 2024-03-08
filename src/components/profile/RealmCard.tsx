a3017854091d457178ba5a47038f72e26ad93eb2
lamachina <lucasmaillard@protonmail.com>
2024-03-08 18:39:27 +0100

    few style changes

--- src/components/profile/RealmCard.tsx
+++ src/components/profile/RealmCard.tsx
@@ -3,40 +3,48 @@ import { useContext, useEffect, useState } from "react"
 import { AppContext } from "@/providers/AppContextProvider";
 import { ImageFromRealmAtomicalId } from "./ImageFromRealmAtomicalId";
 
-import { Card, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
 import { Skeleton } from "@/components/ui/skeleton"
 import { Button } from "@/components/ui/button";
 import { ImageFromData } from "@/components/common/ImageFromData";
 import { DynamicIcon } from "./DynamicIcon";
 import Link from "next/link";
 
-export const RealmCard = ({atomicalId, links, subrealmName, imageData}: {atomicalId?: string, links?: any, subrealmName?: string, imageData: string}) => {
+export const RealmCard = ({ atomicalId, links, subrealmName, imageData }: { atomicalId?: string, links?: any, subrealmName?: string, imageData: string }) => {
 
   return atomicalId?.startsWith('fake-skeleton') ? (
     <Skeleton className="h-[277px] w-full rounded-xl" />
   ) : (
-    <Card className={`flex flex-col items-center`}>
-      <CardHeader>
-        <ImageFromData imageData={imageData}  />
+    <Card className={`flex flex-col items-center w-44`}>
+      <CardHeader className="pb-0" >
+        <ImageFromData imageData={imageData} />
         <div className="flex flex-col items-center justify-around space-y-2">
-          <CardTitle><a href={`/profile/${subrealmName}`} target="_blank">{subrealmName}</a></CardTitle>
+          <CardTitle>
+            <a href={`/profile/${subrealmName}`} target="_blank">{subrealmName}</a>
+          </CardTitle>
           {/* <CardDescription>#{atomicalNumber}</CardDescription> */}
-          <div className="flex flex-row space-x-2 justify-between">
-            {
-              (links && (links.x || links.twitter)) ? (
-                <Link href={links.x || links.twitter} target="_blank">
-                  <DynamicIcon type="x" url={links.x || links.twitter} />
-                </Link>
-              ) : (
-                <></>
-              )
-            }
-            <Link href={`/profile/${subrealmName}`} target="_blank">
-              <DynamicIcon type="person" />
-            </Link>
-          </div>
         </div>
+
       </CardHeader>
+      <CardContent>
+
+      </CardContent>
+      <CardFooter className="w-full flex justify-around ">
+        {
+          (links && (links.x || links.twitter)) ? (
+            <Link href={links.x || links.twitter} target="_blank">
+              <DynamicIcon type="x" url={links.x || links.twitter} />
+            </Link>
+          ) : (
+            <></>
+          )
+        }
+        <Link href={`/profile/${subrealmName}`} target="_blank">
+          <DynamicIcon type="person" />
+        </Link>
+      </CardFooter>
+
+
     </Card>
   )
 }
