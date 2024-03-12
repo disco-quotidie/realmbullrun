f5ab7d390c2c9e0892e4b7d48a75037394478780
disco-quotidie <ccprasong@outlook.com>
2024-03-12 07:08:44 +0000

    collection fix

--- src/components/profile/Collections.tsx
+++ src/components/profile/Collections.tsx
@@ -28,7 +28,7 @@ export const Collections = ({collectionsObject}: {collectionsObject: any}) => {
   }, [collectionsObject])
 
   return (
-    <div className="flex flex-col gap-4 mb-20">
+    <div className="flex flex-col gap-8 mb-20">
       {
         (collections && collections.length > 0) ? (
           <div>Collections</div>
@@ -37,22 +37,22 @@ export const Collections = ({collectionsObject}: {collectionsObject: any}) => {
       {
         collections && collections.map((elem: any) => {
           const { name, image, desc, previews } = elem
-
+          console.log(previews)
           return (
-            <div className="flex flex-col gap-4" key={`${name.toString()}${desc.toString()}`}>
-              <div className="flex flex-row items-center justify-center">
+            <div className="flex flex-col gap-1" key={`${name.toString()}${desc.toString()}`}>
+              <div className="flex flex-row items-center justify-between">
                 <ImageFromData additionalClass="" imageData={image}  />
-                <div className="flex flex-col">
+                <div className="flex flex-col mx-auto">
                   <div>{name.toString()}</div>
                   <div className="max-w-[240px]">{desc.toString()}</div>
                 </div>
               </div>
               <div className="flex flex-row space-x-2 text-center justify-center">
-                {/* {
-                  previews.map((index: any, preview: any) => (
+                {
+                  previews.map((preview: any, index: any) => (
                     <ImageFromData additionalClass="" key={index} imageData={preview.toString()} />
                   ))
-                } */}
+                }
               </div>
               <Button><Link target="_blank" href={`https://wizz.cash/dmint/${name.toLowerCase()}`}>Show on Wizz</Link></Button>
             </div>  
